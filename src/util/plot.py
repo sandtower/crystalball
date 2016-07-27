a59f0d540943b3488160db90fa4cff02fbd4dbde
jordanxlj <jordanxlj@vip.sina.com>
2016-07-27 20:26:37 +0800

    add total asset and shares plot.

--- src/util/plot.py
+++ src/util/plot.py
@@ -4,6 +4,7 @@ matplotlib.use('Agg')
 import matplotlib.dates as mdates
 import matplotlib.finance as mfinance
 import matplotlib.pyplot as mplot
+import numpy as np
 
 from datetime import datetime
 
@@ -20,24 +21,40 @@ class CandlePlot(object):
             self.__xstep = 22
 
     def plot(self, file_name):
-        figure, ax = mplot.subplots()
+        figure, axes = mplot.subplots(nrows=2, ncols=1)
         figure.subplots_adjust(bottom=0.2)
 
-        ax.set_xticks(range(0, len(self.__datas), self.__xstep))
-        ax.set_xticklabels([self.__datas[index]['date'] for index in ax.get_xticks()])
-
-        mfinance.candlestick_ohlc(ax, self.__convert_datas(), width=0.6, colorup='r', colordown='g') 
-
-        ax.xaxis_date()
-        ax.autoscale_view()
+        #candle stick
+        k_ax = axes[0]
+        k_ax.set_xticks(range(0, len(self.__datas), self.__xstep))
+        k_ax.set_xticklabels([self.__datas[index]['date'] for index in k_ax.get_xticks()])
+        mfinance.candlestick_ohlc(k_ax, self.__convert_k_datas(), width=0.6, colorup='r', colordown='g') 
+        k_ax.xaxis_date()
+        k_ax.autoscale_view()
         mplot.setp(mplot.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
 
-        ax.grid(True)
-        mplot.title(self.__stock_code)
+        k_ax.grid(True)
+        #mplot.title(self.__stock_code)
+
+        #asset curve
+        asset_ax = axes[1]
+        asset_ax.set_xticks(range(0, len(self.__datas), self.__xstep))
+        asset_ax.set_xticklabels([self.__datas[index]['date'] for index in asset_ax.get_xticks()])
+
+        ind = np.arange(len(self.__datas))
+        share_ax = asset_ax.twinx()
+        asset_ax.bar(ind, self.__get_asset_datas(), width=0.4, color='y', label='asset')
+        asset_ax.set_ylabel('total asset')
+
+        share_ax.bar(ind+0.4, self.__get_shares_datas(), width=0.4, color='r', label='shares')
+        share_ax.set_ylabel('total share')
+        asset_ax.xaxis_date()
+        asset_ax.autoscale_view()
+        mplot.setp(mplot.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
 
         mplot.savefig(file_name)
 
-    def __convert_datas(self):
+    def __convert_k_datas(self):
         result = []
         index = 0
         for data in self.__datas:
@@ -50,7 +67,19 @@ class CandlePlot(object):
             index += 1
         return result
 
+    def __get_asset_datas(self):
+        result = []
+        for data in self.__datas:
+            result.append(data['totalAssets'])
+        return result
+
+    def __get_shares_datas(self):
+        result = []
+        for data in self.__datas:
+            result.append(data['totalShares'])
+        return result
+
 if __name__ == "__main__":
-    datas = [{'date': '2015-12-01', 'closePrice': 4.88, 'lowestPrice': 4.87, 'openPrice': 4.85, 'highestPrice': 4.79}, {'date': '2015-12-02', 'closePrice': 5.04, 'lowestPrice': 5.03, 'openPrice': 4.87, 'highestPrice': 4.83}, {'date': '2015-12-03', 'closePrice': 5.16, 'lowestPrice': 5.07, 'openPrice': 4.98, 'highestPrice': 4.94}, {'date': '2015-12-04', 'closePrice': 5.06, 'lowestPrice': 4.97, 'openPrice': 5.03, 'highestPrice': 4.95}, {'date': '2015-12-07', 'closePrice': 4.99, 'lowestPrice': 4.96, 'openPrice': 4.96, 'highestPrice': 4.92}, {'date': '2015-12-08', 'closePrice': 4.9, 'lowestPrice': 4.85, 'openPrice': 4.87, 'highestPrice': 4.84}, {'date': '2015-12-09', 'closePrice': 4.88, 'lowestPrice': 4.83, 'openPrice': 4.84, 'highestPrice': 4.8}, {'date': '2015-12-10', 'closePrice': 4.87, 'lowestPrice': 4.83, 'openPrice': 4.84, 'highestPrice': 4.81}, {'date': '2015-12-11', 'closePrice': 4.82, 'lowestPrice': 4.77, 'openPrice': 4.81, 'highestPrice': 4.72}, {'date': '2015-12-14', 'closePrice': 4.85, 'lowestPrice': 4.85, 'openPrice': 4.74, 'highestPrice': 4.71}, {'date': '2015-12-15', 'closePrice': 4.88, 'lowestPrice': 4.83, 'openPrice': 4.84, 'highestPrice': 4.8}, {'date': '2015-12-16', 'closePrice': 4.99, 'lowestPrice': 4.93, 'openPrice': 4.9, 'highestPrice': 4.89}, {'date': '2015-12-17', 'closePrice': 4.99, 'lowestPrice': 4.96, 'openPrice': 4.93, 'highestPrice': 4.91}, {'date': '2015-12-18', 'closePrice': 5.02, 'lowestPrice': 4.94, 'openPrice': 4.95, 'highestPrice': 4.93}, {'date': '2015-12-21', 'closePrice': 5.01, 'lowestPrice': 4.99, 'openPrice': 4.92, 'highestPrice': 4.88}, {'date': '2015-12-22', 'closePrice': 5.01, 'lowestPrice': 4.99, 'openPrice': 5.0, 'highestPrice': 4.95}, {'date': '2015-12-23', 'closePrice': 5.04, 'lowestPrice': 5.0, 'openPrice': 4.99, 'highestPrice': 4.96}, {'date': '2015-12-24', 'closePrice': 5.04, 'lowestPrice': 4.98, 'openPrice': 5.03, 'highestPrice': 4.96}, {'date': '2015-12-25', 'closePrice': 5.0, 'lowestPrice': 4.97, 'openPrice': 4.99, 'highestPrice': 4.94}, {'date': '2015-12-28', 'closePrice': 4.99, 'lowestPrice': 4.86, 'openPrice': 4.97, 'highestPrice': 4.86}, {'date': '2015-12-29', 'closePrice': 4.9, 'lowestPrice': 4.9, 'openPrice': 4.86, 'highestPrice': 4.84}, {'date': '2015-12-30', 'closePrice': 4.94, 'lowestPrice': 4.91, 'openPrice': 4.91, 'highestPrice': 4.87}, {'date': '2015-12-31', 'closePrice': 4.94, 'lowestPrice': 4.9, 'openPrice': 4.91, 'highestPrice': 4.89}, {'date': '2016-01-01', 'closePrice': 4.9, 'lowestPrice': 4.9, 'openPrice': 4.9, 'highestPrice': 4.9}]
+    datas = [{'closePrice': 4.88, 'highestPrice': 4.79, 'openPrice': 4.85, 'totalAssets': 100000, 'date': '2015-12-01', 'totalShares': 1000, 'lowestPrice': 4.87}, {'closePrice': 5.04, 'highestPrice': 4.83, 'openPrice': 4.87, 'totalAssets': 100000, 'date': '2015-12-02', 'totalShares': 1000, 'lowestPrice': 5.03}, {'closePrice': 5.16, 'highestPrice': 4.94, 'openPrice': 4.98, 'totalAssets': 100000, 'date': '2015-12-03', 'totalShares': 1000, 'lowestPrice': 5.07}, {'closePrice': 5.06, 'highestPrice': 4.95, 'openPrice': 5.03, 'totalAssets': 100000, 'date': '2015-12-04', 'totalShares': 1000, 'lowestPrice': 4.97}, {'closePrice': 4.99, 'highestPrice': 4.92, 'openPrice': 4.96, 'totalAssets': 100000, 'date': '2015-12-07', 'totalShares': 1000, 'lowestPrice': 4.96}, {'closePrice': 4.9, 'highestPrice': 4.84, 'openPrice': 4.87, 'totalAssets': 100000, 'date': '2015-12-08', 'totalShares': 1000, 'lowestPrice': 4.85}, {'closePrice': 4.88, 'highestPrice': 4.8, 'openPrice': 4.84, 'totalAssets': 100000, 'date': '2015-12-09', 'totalShares': 1000, 'lowestPrice': 4.83}, {'closePrice': 4.87, 'highestPrice': 4.81, 'openPrice': 4.84, 'totalAssets': 100000, 'date': '2015-12-10', 'totalShares': 1000, 'lowestPrice': 4.83}, {'closePrice': 4.82, 'highestPrice': 4.72, 'openPrice': 4.81, 'totalAssets': 100000, 'date': '2015-12-11', 'totalShares': 1000, 'lowestPrice': 4.77}, {'closePrice': 4.85, 'highestPrice': 4.71, 'openPrice': 4.74, 'totalAssets': 100000, 'date': '2015-12-14', 'totalShares': 1000, 'lowestPrice': 4.85}, {'closePrice': 4.88, 'highestPrice': 4.8, 'openPrice': 4.84, 'totalAssets': 100000, 'date': '2015-12-15', 'totalShares': 1000, 'lowestPrice': 4.83}, {'closePrice': 4.99, 'highestPrice': 4.89, 'openPrice': 4.9, 'totalAssets': 100000, 'date': '2015-12-16', 'totalShares': 1000, 'lowestPrice': 4.93}, {'closePrice': 4.99, 'highestPrice': 4.91, 'openPrice': 4.93, 'totalAssets': 100000, 'date': '2015-12-17', 'totalShares': 1000, 'lowestPrice': 4.96}, {'closePrice': 5.02, 'highestPrice': 4.93, 'openPrice': 4.95, 'totalAssets': 100000, 'date': '2015-12-18', 'totalShares': 1000, 'lowestPrice': 4.94}, {'closePrice': 5.01, 'highestPrice': 4.88, 'openPrice': 4.92, 'totalAssets': 100000, 'date': '2015-12-21', 'totalShares': 1000, 'lowestPrice': 4.99}, {'closePrice': 5.01, 'highestPrice': 4.95, 'openPrice': 5.0, 'totalAssets': 100000, 'date': '2015-12-22', 'totalShares': 1000, 'lowestPrice': 4.99}, {'closePrice': 5.04, 'highestPrice': 4.96, 'openPrice': 4.99, 'totalAssets': 100000, 'date': '2015-12-23', 'totalShares': 1000, 'lowestPrice': 5.0}, {'closePrice': 5.04, 'highestPrice': 4.96, 'openPrice': 5.03, 'totalAssets': 100000, 'date': '2015-12-24', 'totalShares': 1000, 'lowestPrice': 4.98}, {'closePrice': 5.0, 'highestPrice': 4.94, 'openPrice': 4.99, 'totalAssets': 100000, 'date': '2015-12-25', 'totalShares': 1000, 'lowestPrice': 4.97}, {'closePrice': 4.99, 'highestPrice': 4.86, 'openPrice': 4.97, 'totalAssets': 100000, 'date': '2015-12-28', 'totalShares': 1000, 'lowestPrice': 4.86}, {'closePrice': 4.9, 'highestPrice': 4.84, 'openPrice': 4.86, 'totalAssets': 100000, 'date': '2015-12-29', 'totalShares': 1000, 'lowestPrice': 4.9}, {'closePrice': 4.94, 'highestPrice': 4.87, 'openPrice': 4.91, 'totalAssets': 100000, 'date': '2015-12-30', 'totalShares': 1000, 'lowestPrice': 4.91}, {'closePrice': 4.94, 'highestPrice': 4.89, 'openPrice': 4.91, 'totalAssets': 100000, 'date': '2015-12-31', 'totalShares': 1000, 'lowestPrice': 4.9}, {'closePrice': 4.9, 'highestPrice': 4.9, 'openPrice': 4.9, 'totalAssets': 100000, 'date': '2016-01-01', 'totalShares': 1000, 'lowestPrice': 4.9}]
     plot = CandlePlot('002657', datas)
     plot.plot('002657.png')
