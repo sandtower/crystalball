f9f3687a921f9ae04eb0e5af93d4638b94343e29
jordanxlj <jordanxlj@vip.sina.com>
2016-08-01 20:28:39 +0800

    add history tick data local persistence.

--- src/collector/hist_tick_collector.py
+++ src/collector/hist_tick_collector.py
@@ -1,22 +1,73 @@
+from util.config import Config
 from util.db import DB, Collection 
 from util.constants import Constants
 import tushare as ts
+
 from collections import OrderedDict
+import json
+import os
+from util.logger import setup_logger
+setup_logger('test.log')
+
 import logging
 
 _logger = logging.getLogger(__name__)
 
 class HistTickCollector(object):
-    def __init__(self, stock_code, db):
+    SECTION_NAME = 'persistent'
+    ITEM_NAME = 'hist_tick_dir'
+
+    def __init__(self, stock_code, db, config):
         self.__stock_code = stock_code
         self.__db = db
+        self.__config = config
+        self.__base_dir = None
         self.__collection = Collection(stock_code, db)
+        self.__setup()
 
+    def __setup(self):
+        root_dir = config.get_config(self.SECTION_NAME, self.ITEM_NAME)
+        if not root_dir:
+            _logger.warn('hist tick collector get config failed.')
+            return
+
+        if not os.path.exists(root_dir):
+            os.mkdir(root_dir, 0755)
+
+        base_dir = os.path.join(root_dir, self.__stock_code)
+        if not os.path.exists(base_dir):
+            os.mkdir(base_dir, 0755)
+        self.__base_dir = base_dir
+        
     def collect(self, date):
-        df = ts.get_tick_data(self.__stock_code, date)
-        result = self.__parse_dataframe(df)
-        #print result
-        self.__collection.insert_and_update('date', date, **result)
+        if self.__is_exists(date):
+            return self.__load_local_data(date)
+        return self.__fetch_and_save(date)
+
+    def __is_exists(self, date):
+        data_file = os.path.join(self.__base_dir, date)
+        return os.path.exists(data_file) and os.path.isfile(data_file)
+
+    def __load_local_data(self, date):
+        data_file = os.path.join(self.__base_dir, date)
+        file = open(data_file, 'r')
+        try:
+            datas = json.load(file)
+            return OrderedDict(sorted(datas.items(), key= lambda t: t[0]))
+        except Exception as e:
+            _logger.exception(e)
+        file.close()
+
+    def __fetch_and_save(self, date):
+        try:
+            df = ts.get_tick_data(self.__stock_code, date, retry_count=5)
+            result = self.__parse_dataframe(df)
+            print result
+            self.__save_to_file(date, result)
+            return result
+        except Exception as e:
+            _logger.exception(e)
+        return None
 
     def __parse_dataframe(self, df):
         result = {}
@@ -26,27 +77,30 @@ class HistTickCollector(object):
             result[key] = record.to_dict()
         return OrderedDict(sorted(result.items(), key= lambda t: t[0]))
 
+    def __save_to_file(self, date, result):
+        data_file = os.path.join(self.__base_dir, date)
+        with open(data_file, 'w') as outfile:
+            outfile.write(json.dumps(result))
+
     def get_middle_price(self, date):
-        try:
-            df = ts.get_tick_data(self.__stock_code, date=date, retry_count=5)
-            if len(df) > 1:
-                data = self.__get_middle_data(df)
-                _logger.info("get stock(%r), date(%r) middle, time(%r), price(%r)" % (self.__stock_code, date, data[0], data[1]))
-                return data[1]
-        except Exception as e:
-            _logger.exception(e)
+        datas = self.collect(date)
+        if datas and len(datas) > 1:
+            middle_data = self.__get_middle_data(datas)
+            return middle_data[1]
         return None
 
-    def __get_middle_data(self, df):
-        middle = len(df) / 2
-        datas = df.head(middle).tail(1).to_dict()
-        price = datas['price'].values()[0]
-        volume = datas['volume'].values()[0]
-        time = datas['time'].values()[0]
+    def __get_middle_data(self, datas):
+        middle = len(datas) / 2
+        middle_data = datas.items()[middle][1]
+        print middle_data
+        price = middle_data['price']
+        volume = middle_data['volume']
+        time = middle_data['time']
         return time, price, volume
 
 if __name__ == '__main__':
     db = DB(Constants.HIST_TICK_DB_NAME)
-    collector = HistTickCollector('600036', db)
+    config = Config()
+    collector = HistTickCollector('600036', db, config)
     collector.collect('2016-06-28')
     print collector.get_middle_price('2016-06-28')
